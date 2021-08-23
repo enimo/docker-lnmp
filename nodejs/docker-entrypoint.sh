ae1ce2ffaf979704bb9c822c33caaa57fb879814
enimo <enimong@gmail.com>
2021-08-23 20:36:03 +0800

    add nodejs init app

---
+++ nodejs/docker-entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+set -ea
+
+
+if [ "$1" = "npm-test" ]; then
+
+  # if [ ! -f "package.json" ]; then
+
+  #   DATABASE_CLIENT=${DATABASE_CLIENT:-sqlite}
+
+  #   EXTRA_ARGS=${EXTRA_ARGS}
+
+  #   echo "No project found at /srv/app. Creating a new  project"
+
+  #   DOCKER=true vue new . \
+  #     --dbclient=$DATABASE_CLIENT \
+  #     --dbhost=$DATABASE_HOST \
+  #     --dbport=$DATABASE_PORT \
+  #     --dbname=$DATABASE_NAME \
+  #     --dbusername=$DATABASE_USERNAME \
+  #     --dbpassword=$DATABASE_PASSWORD \
+  #     --dbssl=$DATABASE_SSL \
+  #     $EXTRA_ARGSs
+
+  # elif [ ! -d "node_modules" ] || [ ! "$(ls -qAL node_modules 2>/dev/null)" ]; then
+
+  if [ ! -d "node_modules" ] || [ ! "$(ls -qAL node_modules 2>/dev/null)" ]; then
+
+    echo "Node modules not installed. Installing..."
+
+    if [ -f "yarn.lock" ]; then
+
+      yarn install
+
+    else
+
+      npm install
+
+    fi
+
+  fi
+
+fi
+
+echo "Starting your app..."
+
+echo "exec $@"
+exec "$@"
---
+++ strapi/strapi-app-test/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '82de8e2f-7973-4884-9d4e-8e498ac6028d'
+};

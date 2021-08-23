60cef1cde6539633c897bba260f96197c0571d39
enimo <enimong@gmail.com>
2021-08-23 19:36:34 +0800

    rename strapi

--- strapi-app/conf/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '29cb089d776d7d14eff175c7c35469f3'),
-    },
-  },
-});
